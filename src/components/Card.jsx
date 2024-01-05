cea209d6b8183f741b364fd24ffe84939c6d3b23
JSilva723 <silvajonatanivan@gmail.com>
2024-01-05 02:54:06 -0300

    Create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export const Card = ({ name, imgUrl}) => (
+    <div className="relative h-36 w-36">
+        <img src={imgUrl} className="object-cover h-36 w-36 overflow-hidden rounded-xl" />
+        <div className="absolute inset-x-0 bottom-0 h-6 bg-gradient-to-t from-black rounded-b-xl">
+            <h3 className="text-center text-white">{name}</h3>
+        </div>
+    </div>
+)
